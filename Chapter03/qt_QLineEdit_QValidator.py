c68604add1e3cc70106f70e44e63fc6a4224e1b9
sunshe35 <sunshe35@qq.com>
2025-08-12 16:18:47 +0800

    修复3章的QLineEdit_QValidator错误

--- Chapter03/qt_QLineEdit_QValidator.py
+++ Chapter03/qt_QLineEdit_QValidator.py
@@ -58,10 +58,12 @@ class lineEditDemo(QWidget):
         self.setLayout(flo)
 
     def input_validate(self,text, pDoubleValidator, pDoubleLineEdit):
-        if float(text) > pDoubleValidator.top() or float(text) < pDoubleValidator.bottom():
+        if text == '':
+            self.last_text = text
+        elif float(text) > pDoubleValidator.top() or float(text) < pDoubleValidator.bottom():
             pDoubleLineEdit.setText(self.last_text)
         else:
-            self.last_text =text
+            self.last_text = text
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
